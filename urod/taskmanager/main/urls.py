614fad00a9c957293a18185238207c14b1ccd945
polisha <vispolina@icloud.com>
2023-03-09 14:56:47 +0300

    aelkfh

--- urod/taskmanager/main/urls.py
+++ urod/taskmanager/main/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about-us', views.about, name='about'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
     path('create', views.create, name='create')
 ]
--- urod/taskmanager/main/views.py
+++ urod/taskmanager/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Tasks
 from .forms import TasksForm
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
@@ -10,6 +14,52 @@ def index(request):
 
 def about(request):
     return render(request, 'main/lk.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        # username = request.POST.get('username')
+        username = request.POST['username']
+        age = request.POST['age']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.username = username
+
+        myuser.save()
+
+        messages.success(request, "Ура ты новый пользователь приложения! Рады видеть тебя здесь")
+
+        return redirect('signin')
+
+    return render(request, 'main/signup.html')
+
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(usermane=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            username = user.usermane
+            return render(request, "main/lk.html", {"username": username})
+
+        else:
+            messages.error(request, "Те тот пароль, иди вспоминай")
+            return redirect("about")
+
+    return render(request, 'main/signin.html')
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, "ты вышла из аккаунта")
+    return redirect("home")
 
 
 def create(request):
