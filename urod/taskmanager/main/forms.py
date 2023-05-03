6290a43725e363d500d9cb9b4c456f8149ecd5c3
POLISHA <112068683+PolinaVysotskaya@users.noreply.github.com>
2023-05-04 01:07:32 +0300

    trash

--- urod/taskmanager/main/forms.py
+++ urod/taskmanager/main/forms.py
@@ -4,9 +4,18 @@ from django import forms
 
 
 class LoginForm(forms.Form):
+    # fname = forms.CharField(max_length=30, required=True, help_text='Required.')
+    # email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
     username = forms.CharField(label=u'ник')
     pass1 = forms.CharField(label=u'пароль')
     next = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Tasks
+        fields = ('username', 'fname', 'email', 'pass1', 'pass2')
+
+    def save(self):
+        pass
 
 
 class TasksForm(ModelForm): #класс для парня
