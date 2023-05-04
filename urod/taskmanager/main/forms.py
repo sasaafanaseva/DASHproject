f1e5c9b0752b0bf0d5f1cbeac46ab6c5499c8f95
polisha <vispolina@icloud.com>
2023-05-04 19:45:22 +0300

    update2.0

--- urod/taskmanager/main/forms.py
+++ urod/taskmanager/main/forms.py
@@ -4,15 +4,24 @@ from django import forms
 
 
 class LoginForm(forms.Form):
+    # fname = forms.CharField(max_length=30, required=True, help_text='Required.')
+    # email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
     username = forms.CharField(label=u'ник')
     pass1 = forms.CharField(label=u'пароль')
     next = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Tasks
+        fields = ('username', 'fname', 'email', 'pass1', 'pass2')
+
+    def save(self):
+        pass
 
 
 class TasksForm(ModelForm): #класс для парня
     class Meta:
         model = Tasks
-        fields = ["title", "boy_age", "score", "task"]
+        fields = ["title", "boy_age", "size"]
         widgets = {
             "title": TextInput(attrs={
                 'class': 'form-control',
@@ -22,12 +31,8 @@ class TasksForm(ModelForm): #класс для парня
                 'class': 'form-control',
                 'placeholder': 'Введи возраст'
             }),
-            "score": TextInput(attrs={
+            "size": TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Введи баллы'
-            }),
-            "task": Textarea(attrs={
-                'class': 'form-control',
-                'placeholder': 'Его поступок:'
+                'placeholder': 'выбор:'
             }),
         }
--- urod/taskmanager/main/views.py
+++ urod/taskmanager/main/views.py
@@ -1,4 +1,5 @@
 from .forms import LoginForm
+from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
 from django.db.backends import sqlite3
 from django.shortcuts import render, redirect
@@ -11,30 +12,134 @@ from django.contrib.auth import authenticate, login, logout
 import logging
 import sqlite3
 global mydict
+from django.db.models import F
 
-from taskmanager import settings
+# from ..taskmanager import settings
 
+
+#from urod.taskmanager.taskmanager import settings
 
 ## вставлено сашей
 
 def boy_exists(paren): #проверка на существование парня в базе
-    with sqlite3.connect('sqlite_python.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute("""SELECT us_name FROM main_tasks""")
-        df = cursor.fetchall()
-        for i in df:
-            if i[0] == paren:
-                return True
-        return False
+    is_present = Tasks.objects.filter(title=paren).exists()
+    if is_present:
+        return True
+    return False
 
-def add_ball(paren, ball, postupok):  # если парень существует, то обновим его балл
-    with sqlite3.connect('sqlite_python.db') as connection:
+def create_base():
+    try:
+        connection = sqlite3.connect('sqlite_python.db')
         cursor = connection.cursor()
-        cursor.execute(f"""SELECT move FROM main_tasks WHERE us_name = '{paren}';""")
-        pos = cursor.fetchall()[0]
-        post = str(pos)[2:-3] + ', ' + postupok
-        cursor.execute(f"""UPDATE main_tasks SET score = score + {ball}, move = '{post}' WHERE us_name = '{paren}';""")
+        print("База данных создана и успешно подключена к SQLite")
+        cursor.execute(
+            """CREATE TABLE postupok(
+                move text,
+                score int);
+            """)
         connection.commit()
+        cursor.execute("""
+                          INSERT INTO postupok(move, score) VALUES
+                          ('Открыл дверь', 1),
+                          ('Помог расчесать волосы', 1),
+                          ('Сделал комплимент', 1),
+                          ('Уступил место', 2),
+                          ('Обнял при встрече', 2),
+                          ('Помог донести тяжелую сумку', 2),
+                          ('Взял за руку', 3),
+                          ('Выложил твой пост в сторис', 3),
+                          ('Накинул свою куртку в холодный вечер', 3),
+                          ('Помог убраться', 3),
+                          ('Проводил', 3),
+                          ('Подарил цветы', 4),
+                          ('Принес кофе', 4),
+                          ('Встретил после учебы', 4),
+                          ('Починил нерабочий код', 5),
+                          ('Помог с чем-то', 5),
+                          ('Поддержал и выслушал', 5),
+                          ('Подарил плюшевую игрушку', 5),
+                          ('Подвез до дома', 6),
+                          ('Познакомил с друзьями', 6),
+                          ('Скинул дз', 6),
+                          ('Потанцевал со мной', 6),
+                          ('Пригласил погулять', 6),
+                          ('Красиво сфотографировал', 7),
+                          ('Отдал свою футболку/толстовку', 7),
+                          ('Познакомил с родителями', 7),
+                          ('Рассказал смешную шутку', 4),
+                          ('Понес на руках', 7),
+                          ('Подарил духи', 7),
+                          ('Приготовил ужин', 8),
+                          ('Заступился за меня', 8),
+                          ('Сделал массаж', 8),
+                          ('Сходил со мной за одеждой', 8),
+                          ('Оплатил маникюр', 8),
+                          ('Подарил украшение', 8),
+                          ('Достал билеты на концерт любимой группы', 8),
+                          ('Поцеловал', 9),
+                          ('Признался в любви', 9),
+                          ('Обнял и лежал с тобой', 9),
+                          ('Внимательно слушал тебя', 9),
+                          ('Устроил романтическое свидание', 10),
+                          ('Подарил путешествие', 10),
+                          ('Разбудил', -3),
+                          ('Разозлил', -3),
+                          ('Не проявил интерес', -4),
+                          ('Обманул, но признался сам', -4),
+                          ('Критиковал мой образ', -4),
+                          ('Устроил в квартире срач', -4),
+                          ('Не помог', -4),
+                          ('Не подождал', -4),
+                          ('Сравнил с бывшими девушками', -4),
+                          ('Забуллил', -5),
+                          ('Некрасиво пошутил', -5),
+                          ('Забыл забрать тебя', -5),
+                          ('Сильно напился', -5),
+                          ('Не пошел со мной в зал', -5),
+                          ('Поставил свое желание выше моего', -5),
+                          ('Не хочет знакомить с родителями', -5),
+                          ('Флиртовал с другой', -6),
+                          ('Накричал', -6),
+                          ('Опоздал на встречу', -6),
+                          ('Не понял намек', -6),
+                          ('Не разговаривал со мной весь вечер', -6),
+                          ('Ушел на тусовку не предупредив', -6),
+                          ('Попросил счет пополам', -6),
+                          ('Отстранился при друзьях', -6),
+                          ('Сказал что мое хобби бесполезно', -6),
+                          ('Не поздравил с днем рождения', -6),
+                          ('Не захотел разрешить конфликт', -7),
+                          ('Жаловался на жизнь', -7),
+                          ('Плохо относится к твоим друзьям', -7),
+                          ('Жёстко контролил', -7),
+                          ('Обманул и увиливал', -7),
+                          ('Нарушил личные границы', -8),
+                          ('Попросил изменить мою внешность', -8),
+                          ('Не выполнил обещание', -8),
+                          ('Сказал что он всегда прав', -8),
+                          ('Общался с бывшей', -8),
+                          ('Манипулировал', -8),
+                          ('Не захотел проводить время вдвоем', -8),
+                          ('Обвинил меня в своих неудачах', -8),
+                          ('Слился со свидания', -8),
+                          ('Довел до слез', -9),
+                          ('Игнорировал', -9),
+                          ('Бросил', -10),
+                          ('Изменил', -10);
+                        """)
+        connection.commit()
+        # cursor.execute("""select * from po""")
+        # tables = cursor.fetchall()
+        # print(tables)
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при подключении к sqlite", error)
+
+    finally:
+        if (connection):
+            connection.close()
+            print("Соединение с SQLite закрыто")
 
 ## нормальное продолжение полины
 
@@ -96,18 +201,16 @@ def signin(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], pass1=cd['pass1'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return redirect('home')
-                else:
-                    messages.error(request, 'Your account is disabled')
-                    return redirect('home')
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_pass = form.cleaned_data.get('pass1')
+            # raw_pass = make_password(form.cleaned_data.get('pass1'))
+            user = form.save() #вот эти 2 строчки должны решать мою ошибку как говорят все сайты, но оно не работает
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('home')
     else:
         form = LoginForm()
-    return render(request, 'home')
+    return render(request, 'main/signin.html', {'form': form})
 
 
 
@@ -152,20 +255,25 @@ def signout(request):
     return redirect('home')
 
 
+
 def create(request): #создание парня
+    create_base()
     error = ''
     if request.method == 'POST':
         form = TasksForm(request.POST) #имя парня и поступок лежат в переменной form
         if form.is_valid():
-            if boy_exists(form.title):
-                add_ball(form.title, form.score, form.task)
-                return redirect('home')
+            with sqlite3.connect('sqlite_python.db') as connection:
+                cursor = connection.cursor()
+                cursor.execute(f"""select score from postupok WHERE move = '{form.data['size']}';""")
+                ball = int(cursor.fetchall()[0][0]) #получаем балл за поступок
+            if boy_exists(form.data['title']):
+                postupok = str(Tasks.objects.get(title=form.data['title']).size) + "\n" + form.data['size']
+                Tasks.objects.filter(title=form.data['title']).update(score=F("score") + ball, size = postupok)
+                messages.success(request, "твой парень уже есть в базе, ему добавлен новый поступок")
             else:
-                form.save()
-                return redirect('home')
-                # my_boy(form.title, form.boy_age, form.score, form.task)
-            # form.save()
-            # return redirect('home')
+                Tasks.objects.create(title = form.data['title'], boy_age = form.data['boy_age'], score = ball, size = form.data['size'])
+                messages.success(request, "ты его единственная, парень добавлен в базу")
+            return redirect('home')
         else:
             error = "введи нормально пжпж"
 
