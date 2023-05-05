3350726b33610bc3f11267b212c9db87bc2ca20e
polisha <vispolina@icloud.com>
2023-05-05 13:31:12 +0300

    update3.0

--- urod/taskmanager/main/urls.py
+++ urod/taskmanager/main/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about-us', views.about, name='about'),
+    path('account', views.about, name='about'),
+    path('rating-global', views.rating, name='rating'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create') ##(вид в поисковой строке, обращение, функция во views)
 ]
