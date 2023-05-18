a025fdd981045923bf25fa8a4db7764143076f93
sasaafanaseva <112332319+sasaafanaseva@users.noreply.github.com>
2023-05-18 15:24:51 +0300

    Update settings.py

--- dashweb/taskmanager/taskmanager/settings.py
+++ dashweb/taskmanager/taskmanager/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlitebaseparni',
+        'NAME': BASE_DIR / 'db.sqlitebaseparnii',
     }
 }
 
