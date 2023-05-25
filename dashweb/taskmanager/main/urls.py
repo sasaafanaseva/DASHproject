12844e0c0a2933f158de98927dcdfa57f26bfad0
sasaafanaseva <112332319+sasaafanaseva@users.noreply.github.com>
2023-05-25 15:02:25 +0300

    Update urls.py

--- dashweb/taskmanager/main/urls.py
+++ dashweb/taskmanager/main/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
     path('deeds/<str:title>', views.deed, name='deed'),
-    path('create', views.create, name='create'),##(вид в поисковой строке, обращение, функция во views)
+    path('add/<str:title>', views.add, name='add'),
+    path('create', views.create, name='create'), ##(вид в поисковой строке, обращение, функция во views)
     path('comments/<int:id>', views.comments, name='comments')
 ]
